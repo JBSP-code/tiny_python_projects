dd275b5e602de82f792650f409ce523ba92dd909
theFippo <schmid.jeffrey@gmail.com>
2023-01-02 16:17:00 +0100

    ch02: create initial program

---
+++ 02_crowsnest/crowsnest.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+"""
+Author : Jeffrey Schmid-Paz
+Date   : 2023-01-02
+Purpose: Warn captain for a something near the ship
+"""
+
+import argparse
+
+
+# --------------------------------------------------
+def get_args():
+    """Get command-line arguments"""
+
+    parser = argparse.ArgumentParser(
+        description="Crow's Nest -- choose the correct article",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+
+    parser.add_argument("word", metavar="word", help="A word")
+
+    return parser.parse_args()
+
+
+# --------------------------------------------------
+def main():
+    """Run the program"""
+
+    args = get_args()
+    pos_arg = args.positional
+
+    print(f'positional = "{pos_arg}"')
+
+
+# --------------------------------------------------
+if __name__ == "__main__":
+    main()
