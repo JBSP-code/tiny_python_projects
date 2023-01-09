36572e312366991f65da3e834daa79fbb9b1e6bb
theFippo <schmid.jeffrey@gmail.com>
2023-01-09 18:27:24 +0100

    ch03: modify program to align help message

--- 03_picnic/picnic.py
+++ 03_picnic/picnic.py
@@ -17,36 +17,9 @@ def get_args():
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
     )
 
-    parser.add_argument("positional", metavar="str", help="A positional argument")
+    parser.add_argument("positional", metavar="str", help="Item(s) to bring")
 
-    parser.add_argument(
-        "-a",
-        "--arg",
-        help="A named string argument",
-        metavar="str",
-        type=str,
-        default="",
-    )
-
-    parser.add_argument(
-        "-i",
-        "--int",
-        help="A named integer argument",
-        metavar="int",
-        type=int,
-        default=0,
-    )
-
-    parser.add_argument(
-        "-f",
-        "--file",
-        help="A readable file",
-        metavar="FILE",
-        type=argparse.FileType("rt"),
-        default=None,
-    )
-
-    parser.add_argument("-o", "--on", help="A boolean flag", action="store_true")
+    parser.add_argument("-s", "--sorted", help="Sort the items", action="store_true")
 
     return parser.parse_args()
 
@@ -56,17 +29,11 @@ def main():
     """Make a jazz noise here"""
 
     args = get_args()
-    str_arg = args.arg
-    int_arg = args.int
-    file_arg = args.file
-    flag_arg = args.on
-    pos_arg = args.positional
+    sort_flag = args.on
+    items = args.str
 
-    print(f'str_arg = "{str_arg}"')
-    print(f'int_arg = "{int_arg}"')
-    print('file_arg = "{}"'.format(file_arg.name if file_arg else ""))
-    print(f'flag_arg = "{flag_arg}"')
-    print(f'positional = "{pos_arg}"')
+    print(f'flag_arg = "{sort_flag}"')
+    print(f'positional = "{items}"')
 
 
 # --------------------------------------------------
