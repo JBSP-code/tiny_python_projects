abdbe66d235fc636b182f3d2f259dd70b2e8ba9e
theFippo <schmid.jeffrey@gmail.com>
2023-01-23 18:39:25 +0100

    ch04: valid solution

--- 04_jump_the_five/jump.py
+++ 04_jump_the_five/jump.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """
-Author : Anonymous <Anonymous@localhost>
+Author : Jeffrey Paz-Schmid
 Date   : 2023-01-22
-Purpose: Rock the Casbah
+Purpose: Encode Phone-Numbers
 """
 
 import argparse
@@ -13,38 +13,11 @@ def get_args():
     """Get command-line arguments"""
 
     parser = argparse.ArgumentParser(
-        description='Rock the Casbah',
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+        description="Jump the Five",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
 
-    parser.add_argument('positional',
-                        metavar='str',
-                        help='A positional argument')
-
-    parser.add_argument('-a',
-                        '--arg',
-                        help='A named string argument',
-                        metavar='str',
-                        type=str,
-                        default='')
-
-    parser.add_argument('-i',
-                        '--int',
-                        help='A named integer argument',
-                        metavar='int',
-                        type=int,
-                        default=0)
-
-    parser.add_argument('-f',
-                        '--file',
-                        help='A readable file',
-                        metavar='FILE',
-                        type=argparse.FileType('rt'),
-                        default=None)
-
-    parser.add_argument('-o',
-                        '--on',
-                        help='A boolean flag',
-                        action='store_true')
+    parser.add_argument("positional", metavar="str", help="Input text")
 
     return parser.parse_args()
 
@@ -54,19 +27,28 @@ def main():
     """Make a jazz noise here"""
 
     args = get_args()
-    str_arg = args.arg
-    int_arg = args.int
-    file_arg = args.file
-    flag_arg = args.on
     pos_arg = args.positional
 
-    print(f'str_arg = "{str_arg}"')
-    print(f'int_arg = "{int_arg}"')
-    print('file_arg = "{}"'.format(file_arg.name if file_arg else ''))
-    print(f'flag_arg = "{flag_arg}"')
-    print(f'positional = "{pos_arg}"')
+    jumper = {
+        "1": "9",
+        "2": "8",
+        "3": "7",
+        "4": "6",
+        "5": "0",
+        "6": "4",
+        "7": "3",
+        "8": "2",
+        "9": "1",
+        "0": "5",
+    }
+
+    for char in pos_arg:
+        if char in jumper:
+            print(jumper[char], end="")
+        else:
+            print(char, end="")
 
 
 # --------------------------------------------------
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
